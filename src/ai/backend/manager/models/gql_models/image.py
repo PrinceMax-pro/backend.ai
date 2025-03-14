74b912ba987a8d2f5804ae60d28b72db791a5596
jopemachine <jopemachine@naver.com>
2025-03-14 04:31:33 +0000

    fix: Rename response type to `PurgeImagesPayload`

--- src/ai/backend/manager/models/gql_models/image.py
+++ src/ai/backend/manager/models/gql_models/image.py
@@ -1153,7 +1153,7 @@ class PurgeImagesOptions(graphene.InputObjectType):
     )
 
 
-class PurgeImagesResponse(graphene.ObjectType):
+class PurgeImagesPayload(graphene.ObjectType):
     """
     Added in 25.5.0.
     """
@@ -1171,7 +1171,7 @@ class PurgeImages(graphene.Mutation):
         key = PurgeImagesKey(required=True)
         options = PurgeImagesOptions(default_value={"force": False, "noprune": False})
 
-    Output = PurgeImagesResponse
+    Output = PurgeImagesPayload
 
     @staticmethod
     async def mutate(
