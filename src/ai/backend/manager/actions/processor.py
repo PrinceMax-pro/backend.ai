f35f0e00db92887630f5dfc100cb2417f81a92ec
jopemachine <jopemachine@naver.com>
2025-03-19 07:32:50 +0000

    fix: Add skeleton files

--- src/ai/backend/manager/actions/processor.py
+++ src/ai/backend/manager/actions/processor.py
@@ -1,8 +1,13 @@
-import asyncio
+import uuid
+from abc import ABC
 from datetime import datetime
 from typing import Awaitable, Callable, Generic, Optional
 
+from ai.backend.common.bgtask import BackgroundTaskManager, ProgressReporter
+from ai.backend.common.types import DispatchResult
+
 from .action import (
+    BaseAction,
     BaseActionResultMeta,
     ProcessResult,
     TAction,
@@ -11,9 +16,20 @@ from .action import (
 from .monitors.monitor import ActionMonitor
 
 
+class ActionValidator(ABC):
+    async def validate(self, action: BaseAction) -> None:
+        pass
+
+
 class ActionProcessor(Generic[TAction, TActionResult]):
     _monitors: list[ActionMonitor]
     _func: Callable[[TAction], Awaitable[TActionResult]]
+
+    # TODO: Implement this.
+    # _validators: list[ActionValidator]
+
+    # TODO: Implement this.
+    # rollback: Callable[[TAction], None]
 
     def __init__(
         self,
@@ -26,24 +42,32 @@ class ActionProcessor(Generic[TAction, TActionResult]):
     async def _run(self, action: TAction) -> ProcessResult[TActionResult]:
         started_at = datetime.now()
         status: str
+        exc: Optional[Exception] = None
         try:
             result = await self._func(action)
             status = "success"
             description = "Success"
         except Exception as e:
+            exc = e
+            result = None
             status = "error"
             description = str(e)
-        finally:
-            end_at = datetime.now()
-            duration = (end_at - started_at).total_seconds()
-            meta = BaseActionResultMeta(
-                status=status,
-                description=description,
-                started_at=started_at,
-                end_at=end_at,
-                duration=duration,
-            )
+
+        end_at = datetime.now()
+        duration = (end_at - started_at).total_seconds()
+        meta = BaseActionResultMeta(
+            status=status,
+            description=description,
+            started_at=started_at,
+            end_at=end_at,
+            duration=duration,
+        )
+
+        if result:
             return ProcessResult(meta, result)
+        else:
+            assert exc is not None
+            raise exc
 
     async def wait_for_complete(self, action: TAction) -> TActionResult:
         for monitor in self._monitors:
@@ -53,5 +77,12 @@ class ActionProcessor(Generic[TAction, TActionResult]):
             await monitor.done(action, result)
         return result.result
 
-    async def fire_and_forget(self, action: TAction) -> None:
-        asyncio.create_task(self.wait_for_complete(action))
+    # TODO: background_task_manager를 ActionProcessor 생성자에서 받아오는 게 나은지?
+    async def fire_and_forget(
+        self, background_task_manager: BackgroundTaskManager, action: TAction
+    ) -> uuid.UUID:
+        async def _bg_task(reporter: ProgressReporter) -> DispatchResult:
+            result = await self.wait_for_complete(action)
+            return result.to_bgtask_result()
+
+        return await background_task_manager.start(_bg_task)
