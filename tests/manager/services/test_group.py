7540aef40d83d97d942f3f1b11cf12b832125812
BoGyum Kim | 김보겸 <bkkim@lablup.com>
2025-03-27 17:36:43 +0900

    refactor: Make group creation fixture using asynccontextmanager

--- tests/manager/services/test_group.py
+++ tests/manager/services/test_group.py
@@ -1,6 +1,7 @@
 import uuid
+from contextlib import asynccontextmanager
 from datetime import datetime
-from typing import Awaitable, Callable, Optional
+from typing import Any, AsyncGenerator, Optional
 from unittest.mock import MagicMock
 
 import pytest
@@ -45,38 +46,36 @@ def processors(database_fixture, database_engine, mock_storage_manager) -> Group
     return GroupProcessors(group_service)
 
 
-@pytest.fixture
+@asynccontextmanager
 async def create_group(
     database_engine: ExtendedAsyncSAEngine,
-) -> Callable[..., Awaitable[uuid.UUID]]:
-    """
-    NOTICE: To use 'default' resource policy, you must use `database_fixture` concurrently in test function
-    """
+    *,
+    domain_name: str,
+    name: str,
+    type: ProjectType = ProjectType.GENERAL,
+    resource_policy_name: str = "default",
+) -> AsyncGenerator[Any, Any]:
+    # NOTICE: To use 'default' resource policy, you must use `database_fixture` concurrently in test function
+    async with database_engine.begin_session() as session:
+        group_id = uuid.uuid4()
+        group_data = {
+            "id": group_id,
+            "name": name,
+            "description": "Test group",
+            "is_active": True,
+            "domain_name": domain_name,
+            "total_resource_slots": {},
+            "allowed_vfolder_hosts": {},
+            "resource_policy": resource_policy_name,
+            "type": type,
+        }
+        await session.execute(sa.insert(GroupRow).values(group_data))
 
-    async def _create_group(
-        domain_name: str,
-        name: str,
-        type: ProjectType = ProjectType.GENERAL,
-        resource_policy_name: str = "default",
-    ) -> uuid.UUID:
-        async with database_engine.begin() as conn:
-            group_id = uuid.uuid4()
-            group_data = {
-                "id": group_id,
-                "name": name,
-                "description": "Test group",
-                "is_active": True,
-                "domain_name": domain_name,
-                "total_resource_slots": {},
-                "allowed_vfolder_hosts": {},
-                "resource_policy": resource_policy_name,
-                "type": type,
-            }
-            await conn.execute(sa.insert(GroupRow).values(group_data))
-            await conn.execute(sa.select(GroupRow).where(GroupRow.id == group_id))
-            return group_id
-
-    return _create_group
+    try:
+        yield group_id
+    finally:
+        async with database_engine.begin_session() as session:
+            await session.execute(sa.delete(GroupRow).where(GroupRow.id == group_id))
 
 
 @pytest.mark.asyncio
@@ -136,67 +135,30 @@ async def test_create_group(
 
 
 @pytest.mark.asyncio
-# @pytest.mark.parametrize(
-#     "test_scenario",
-#     [
-#         TestScenario.success(
-#             "Modify a group",
-#             ModifyGroupAction(
-#                 name="test_modify_group",
-#                 description="description modified",
-#                 is_active=False,
-#             ),
-#             ModifyGroupActionResult(
-#                 data=GroupData(
-#                     id=uuid.uuid4(),
-#                     name="test_modfiy_group",
-#                     description="description modified",
-#                     is_active=False,
-#                     created_at=datetime.now(),
-#                     modified_at=datetime.now(),
-#                     integration_id="",
-#                     domain_name="default",
-#                     total_resource_slots=ResourceSlot.from_user_input({}, None),
-#                     allowed_vfolder_hosts=VFolderHostPermissionMap({}),
-#                     dotfiles=b"\x90",
-#                     resource_policy="default",
-#                     type=ProjectType.GENERAL,
-#                     container_registry={},
-#                 ),
-#                 success=True,
-#             ),
-#         )
-#     ]
-# )
 async def test_modify_group(
     processors: GroupProcessors,
-    # test_scenario: TestScenario[ModifyGroupAction, ModifyGroupActionResult],
-    create_group: Callable[..., Awaitable[uuid.UUID]],
+    database_engine: ExtendedAsyncSAEngine,
 ) -> None:
-    # TODO: Implement functionality similar to JUnit's @BeforeEach and @AfterEach annotations.
-    # TODO: Implement test utils dynamically inject values into parameters
+    async with create_group(
+        database_engine=database_engine, domain_name="default", name="test_modfiy_group"
+    ) as group_id:
+        action = ModifyGroupAction(
+            group_id=group_id,
+            name="modified_name",
+            description="modified description",
+            is_active=False,
+        )
+        result: ModifyGroupActionResult = await processors.modify_group.wait_for_complete(action)
+        group_data: Optional[GroupData] = result.data
 
-    # await test_scenario.test(processors.modify_group.wait_for_complete)
-
-    group_id = await create_group(domain_name="default", name="test_modfiy_group")
-    action = ModifyGroupAction(
-        group_id=group_id,
-        name="modified_name",
-        description="modified description",
-        is_active=False,
-    )
-    result: ModifyGroupActionResult = await processors.modify_group.wait_for_complete(action)
-    group_data: Optional[GroupData] = result.data
-
-    assert group_data is not None
-    assert group_data.name == "modified_name"
-    assert group_data.description == "modified description"
-    assert group_data.is_active == False  # noqa: E712
+        assert group_data is not None
+        assert group_data.name == "modified_name"
+        assert group_data.description == "modified description"
+        assert group_data.is_active == False  # noqa: E712
 
 
 async def test_modify_group_with_invalid_group_id(
     processors: GroupProcessors,
-    create_group: Callable[..., Awaitable[uuid.UUID]],
 ) -> None:
     action = ModifyGroupAction(
         group_id=uuid.UUID("00000000-0000-0000-0000-000000000000"),
@@ -210,114 +172,65 @@ async def test_modify_group_with_invalid_group_id(
 
 
 @pytest.mark.asyncio
-# @pytest.mark.parametrize(
-#     "test_scenario",
-#     [
-#         TestScenario.success(
-#             "delete a group",
-#             DeleteGroupAction(
-#                 group_id=uuid.uuid4()
-#             ),
-#             DeleteGroupActionResult(
-#                 data=None,
-#                 success=True
-#             )
-#         ),
-#         TestScenario.success(
-#             "delete a group with invalid group_id",
-#             DeleteGroupAction(
-#                 group_id=uuid.UUID("00000000-0000-0000-0000-000000000000")
-#             ),
-#             DeleteGroupActionResult(
-#                 data=None,
-#                 success=False
-#             )
-#         )
-#     ]
-# )
 async def test_delete_group(
     processors: GroupProcessors,
-    # test_scenario: TestScenario[ModifyGroupAction, ModifyGroupActionResult],
-    create_group: Callable[..., Awaitable[uuid.UUID]],
+    database_engine: ExtendedAsyncSAEngine,
 ) -> None:
-    # await test_scenario.test(processors.delete_group.wait_for_complete)
-    group_id = await create_group(domain_name="default", name="test_delete_group")
-    action = DeleteGroupAction(group_id=group_id)
-    result: DeleteGroupActionResult = await processors.delete_group.wait_for_complete(action)
-    assert result.data is None
-    assert result.success == True  # noqa: E712
+    async with create_group(
+        database_engine=database_engine, domain_name="default", name="test_delete_group"
+    ) as group_id:
+        action = DeleteGroupAction(group_id=group_id)
+        result: DeleteGroupActionResult = await processors.delete_group.wait_for_complete(action)
+        assert result.data is None
+        assert result.success == True  # noqa: E712
 
 
 @pytest.mark.asyncio
 async def test_delete_group_in_db(
     processors: GroupProcessors,
-    create_group: Callable[..., Awaitable[uuid.UUID]],
     database_engine: ExtendedAsyncSAEngine,
 ) -> None:
-    group_id = await create_group(domain_name="default", name="test_delete_group_in_db")
-    action = DeleteGroupAction(group_id=group_id)
-    await processors.delete_group.wait_for_complete(action)
-    async with database_engine.begin_session() as session:
-        group = await session.scalar(sa.select(GroupRow).where(GroupRow.id == group_id))
+    async with create_group(
+        database_engine=database_engine, domain_name="default", name="test_delete_group_in_db"
+    ) as group_id:
+        action = DeleteGroupAction(group_id=group_id)
+        await processors.delete_group.wait_for_complete(action)
+        async with database_engine.begin_session() as session:
+            group = await session.scalar(sa.select(GroupRow).where(GroupRow.id == group_id))
 
-        assert group is not None
-        assert group.is_active == False  # noqa: E712
-        assert group.integration_id is None
+            assert group is not None
+            assert group.is_active == False  # noqa: E712
+            assert group.integration_id is None
 
 
 @pytest.mark.asyncio
-# @pytest.mark.parametrize(
-#     "test_scenario",
-#     [
-#         TestScenario.success(
-#             "Purge a group",
-#             PurgeGroupAction(
-#                 group_id=uuid.uuid4()
-#             ),
-#             PurgeGroupActionResult(
-#                 data=None,
-#                 success=True
-#             )
-#         ),
-#         TestScenario.success(
-#             "Purge a group with invalid group_id",
-#             PurgeGroupAction(
-#                 group_id=uuid.UUID("00000000-0000-0000-0000-000000000000")
-#             ),
-#             PurgeGroupActionResult(
-#                 data=None,
-#                 success=False
-#             )
-#         )
-#     ]
-# )
 async def test_purge_group(
     processors: GroupProcessors,
-    # test_scenario: TestScenario[ModifyGroupAction, ModifyGroupActionResult],
-    create_group: Callable[..., Awaitable[uuid.UUID]],
+    database_engine: ExtendedAsyncSAEngine,
 ) -> None:
-    # await test_scenario.test(processors.purge_group.wait_for_complete)
+    async with create_group(
+        database_engine=database_engine, domain_name="default", name="test_purge_group"
+    ) as group_id:
+        await processors.delete_group.wait_for_complete(DeleteGroupAction(group_id))
 
-    group_id = await create_group(domain_name="default", name="test_purge_group")
-    await processors.delete_group.wait_for_complete(DeleteGroupAction(group_id))
-
-    result: PurgeGroupActionResult = await processors.purge_group.wait_for_complete(
-        PurgeGroupAction(group_id)
-    )
-    assert result.data is None
-    assert result.success == True  # noqa: E712
+        result: PurgeGroupActionResult = await processors.purge_group.wait_for_complete(
+            PurgeGroupAction(group_id)
+        )
+        assert result.data is None
+        assert result.success == True  # noqa: E712
 
 
 @pytest.mark.asyncio
 async def test_purge_group_in_db(
     processors: GroupProcessors,
-    create_group: Callable[..., Awaitable[uuid.UUID]],
     database_engine: ExtendedAsyncSAEngine,
 ) -> None:
-    group_id = await create_group(domain_name="default", name="test_purge_group_in_db")
-    await processors.delete_group.wait_for_complete(DeleteGroupAction(group_id))
-    await processors.purge_group.wait_for_complete(PurgeGroupAction(group_id))
+    async with create_group(
+        database_engine=database_engine, domain_name="default", name="test_purge_group_in_db"
+    ) as group_id:
+        await processors.delete_group.wait_for_complete(DeleteGroupAction(group_id))
+        await processors.purge_group.wait_for_complete(PurgeGroupAction(group_id))
 
-    async with database_engine.begin_session() as session:
-        group = await session.scalar(sa.select(GroupRow).where(GroupRow.id == group_id))
-        assert group is None
+        async with database_engine.begin_session() as session:
+            group = await session.scalar(sa.select(GroupRow).where(GroupRow.id == group_id))
+            assert group is None
